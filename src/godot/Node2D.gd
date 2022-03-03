8f9f6be6819112e5d46fd28df9ff33659e338c41
Alexandre Fonseca <80794067+Xandebrabe@users.noreply.github.com>
2022-03-03 14:56:24 -0300

    Commit trying to apply buttons

--- src/godot/Node2D.gd
+++ src/godot/Node2D.gd
@@ -1,18 +1,13 @@
 extends Node2D
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-var deslocamento = 0
-var botao = 0 
-# Called when the node enters the scene tree for the first time.(apenas uma vez)
-func _ready():
-	$Label.text = "boa noite bruno aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
-	#pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-#O sistema de coordenadas nos jogos eh invertido no eixo y
+var contador = 0
+func _on_NExt_pressed():
+	contador += 1
+	if contador % 2 == 1:
+		$"Aula1/Tela Preta".visible = true
+		$"Aula1/Parece azul".visible = false
+	elif contador % 2 == 0:
+		$"Aula1/Tela Preta".visible = false
+		$"Aula1/Parece azul".visible = true
+		
