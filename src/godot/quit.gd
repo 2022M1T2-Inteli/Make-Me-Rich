198d00cb352cff33b8413db7866333df0cd4ce60
pingu01 <99196159+pingu01@users.noreply.github.com>
2022-03-07 20:24:02 -0300

    main menu

---
+++ src/godot/quit.gd
@@ -0,0 +1,5 @@
+extends Button
+
+
+func _on_quit_pressed():
+	get_tree().quit()
---
+++ src/godot/start.gd
@@ -0,0 +1,14 @@
+extends Button
+
+
+
+func _on_start_mouse_entered():
+	pass # Replace with function body.
+
+
+func _on_start_mouse_exited():
+	pass # Replace with function body.
+
+
+func _on_start_pressed():
+	get_tree().change_scene("res://Node2D.tscn")
