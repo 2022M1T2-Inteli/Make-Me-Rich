8f81dbff11fa5f37ce29571c08c5745dc10715be
pingu01 <99196159+pingu01@users.noreply.github.com>
2022-03-09 15:38:24 -0300

    menu updated

---
+++ src/godot/new.gd
@@ -0,0 +1,18 @@
+extends Button
+
+
+#quando o mouse passa por cima do botao, a cor dele muda
+func _on_new_mouse_entered():
+	$botao_new.modulate.r = 0.7
+	$botao_new.modulate.g = 0.7
+	$botao_new.modulate.b = 0.7
+	
+# a cor dele volta ao normal
+func _on_new_mouse_exited():
+	$botao_new.modulate.r = 1
+	$botao_new.modulate.g = 1
+	$botao_new.modulate.b = 1
+
+#quando clica no botao a cena muda
+func _on_new_pressed():
+	get_tree().change_scene("res://Node2D.tscn")
--- src/godot/start.gd
+++ src/godot/start.gd
@@ -3,12 +3,16 @@ extends Button
 
 #quando o mouse passa por cima do botao, a cor dele muda
 func _on_start_mouse_entered():
-	pass # Replace with function body.
-
+	$botao_start.modulate.r = 0.7
+	$botao_start.modulate.g = 0.7
+	$botao_start.modulate.b = 0.7
+	
 # a cor dele volta ao normal
 func _on_start_mouse_exited():
-	pass # Replace with function body.
+	$botao_start.modulate.r = 1
+	$botao_start.modulate.g = 1
+	$botao_start.modulate.b = 1
 
 #quando clica no botao a cena muda
 func _on_start_pressed():
-	get_tree().change_scene("res://Node2D.tscn")
+	get_tree().change_scene("res://load_new.tscn")
