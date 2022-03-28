a6b6812f068083b59701f2f78df455832adfda95
pingu01 <99196159+pingu01@users.noreply.github.com>
2022-03-28 20:52:42 -0300

    botoes investir

--- src/godot/Investir.gd
+++ src/godot/Investir.gd
@@ -1,7 +1,7 @@
 extends Node2D
 var C = Global.dinheiro
 var i = 0.25
-var J = C*i*Global.dia
+var J = C*i*(Global.dia - 1)
 
 #mostra o dinheiro na tela 
 func _ready():
@@ -23,3 +23,27 @@ func _on_perderDinheiro_pressed():
 #volta pra tela inicial
 func _on_voltar_pressed():
 	get_tree().change_scene("res://Cena_computador.tscn")
+
+
+func _on_ganharDinheiro_mouse_entered():
+	$computador/Depositar.modulate.r = 0.7
+	$computador/Depositar.modulate.g = 0.7
+	$computador/Depositar.modulate.b = 0.7
+
+
+func _on_ganharDinheiro_mouse_exited():
+	$computador/Depositar.modulate.r = 1
+	$computador/Depositar.modulate.g = 1
+	$computador/Depositar.modulate.b = 1
+
+
+func _on_perderDinheiro_mouse_entered():
+	$computador/Sacar.modulate.r = 0.7
+	$computador/Sacar.modulate.g = 0.7
+	$computador/Sacar.modulate.b = 0.7
+
+
+func _on_perderDinheiro_mouse_exited():
+	$computador/Sacar.modulate.r = 1
+	$computador/Sacar.modulate.g = 1
+	$computador/Sacar.modulate.b = 1
