47d207f5a90c36d7150b94a6371f936e6b3ca378
pingu01 <99196159+pingu01@users.noreply.github.com>
2022-03-29 15:04:01 -0300

    investimento corrigido

--- src/godot/Investir.gd
+++ src/godot/Investir.gd
@@ -2,24 +2,34 @@ extends Node2D
 var C = Global.dinheiro
 var i = 0.25
 var J = C*i*(Global.dia - 1)
-
+var investir = 0
 #mostra o dinheiro na tela 
+
 func _ready():
 	$computador/dinheiroPC.text = "R$" + str(Global.dinheiro)
 	$computador/conta.text = "R$" + str(Global.conta)
+
+
 #ganha dinheiro
 func _on_ganharDinheiro_pressed():
-	Global.conta += float($computador/extrairDinheiro.text)
-	Global.dinheiro -= float($computador/extrairDinheiro.text)
 	Global.conta += J
 	$computador/dinheiroPC.text = "R$" + str(Global.dinheiro)
 	$computador/conta.text = "R$" + str(Global.conta)
+	if Global.dinheiro >= float($computador/extrairDinheiro.text):
+		Global.conta += float($computador/extrairDinheiro.text)
+		Global.dinheiro -= float($computador/extrairDinheiro.text)
+		$computador/dinheiroPC.text = "R$" + str(Global.dinheiro)
+		$computador/conta.text = "R$" + str(Global.conta)
+		
 	#dinheiro volta pra mao
 func _on_perderDinheiro_pressed():
-	Global.conta -= float($computador/mao.text)
-	Global.dinheiro += float($computador/mao.text)
 	$computador/dinheiroPC.text = "R$" + str(Global.dinheiro)
 	$computador/conta.text = "R$" + str(Global.conta)
+	if Global.conta >= float($computador/mao.text):
+		Global.conta -= float($computador/mao.text)
+		Global.dinheiro += float($computador/mao.text)
+		$computador/dinheiroPC.text = "R$" + str(Global.dinheiro)
+		$computador/conta.text = "R$" + str(Global.conta)
 #volta pra tela inicial
 func _on_voltar_pressed():
 	get_tree().change_scene("res://Cena_computador.tscn")
