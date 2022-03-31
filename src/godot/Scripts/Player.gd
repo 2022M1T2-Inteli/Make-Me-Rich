c3a4166d3b316a7d8dd06740d8574598fff3ddfb
pingu01 <99196159+pingu01@users.noreply.github.com>
2022-03-31 09:44:02 -0300

    bug do cassino corrigido

--- src/godot/Scripts/Player.gd
+++ src/godot/Scripts/Player.gd
@@ -1,23 +1,21 @@
 extends Panel
 
-var money = Global.dinheiro
-
 func _ready():
 	refresh_label()
 
 
 func refresh_label():
-	get_node("CenterContainer/Label").text = ("Money: " + String(money))
+	$CenterContainer/Label.text = ("Money: " + String(Global.dinheiro))
 
 func add_money(gain):
-	money += gain
+	Global.dinheiro += gain
 	refresh_label()
 
 func sub_money(loss):
-	money -= loss
-	if money < 0: money = 0
+	Global.dinheiro -= loss
+	if Global.dinheiro < 0: Global.dinheiro = 0
 	refresh_label()
 
 func can_pay(cost):
-	var can = (money - cost) >= 0
+	var can = (Global.dinheiro - cost) >= 0
 	return can
